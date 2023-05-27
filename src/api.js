c2cb70a97a244e8c25418891afd3db68e7d3f4c2
Anderson Silva <akgs_7@hotmail.com>
2023-05-26 23:00:58 -0300

    Refatoração do código do arquivo API para linkar a variável de ambiente.

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-    baseURL: 'https://site-personal-api.onrender.com/'
+  baseURL: apiUrl,
 });
 
 export default api;
